27593a320194e18e8fa71a5b350d19a821ed3dc3
Jongmin Choi <138505370+JongminChoi98@users.noreply.github.com>
2025-02-07 14:54:17 -0800

    [ADD] Header Navigation component - (Shadcn UI)

---
+++ app/common/components/navigation.tsx
@@ -0,0 +1,282 @@
+import {
+  BarChart3Icon,
+  BellIcon,
+  LogOutIcon,
+  MessageCircleIcon,
+  SettingsIcon,
+  UserIcon,
+} from "lucide-react";
+import { Link } from "react-router";
+import { cn } from "~/lib/utils";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "./ui/navigation-menu";
+import { Separator } from "./ui/separator";
+
+const menus = [
+  {
+    name: "Products",
+    to: "/products",
+    items: [
+      {
+        name: "Leaderboards",
+        description: "See the top performers in your community",
+        to: "/products/leaderboards",
+      },
+      {
+        name: "Categories",
+        description: "See the categories in your community",
+        to: "/products/categories",
+      },
+      {
+        name: "Search",
+        description: "Search for a product",
+        to: "/products/search",
+      },
+      {
+        name: "Submit a Product",
+        description: "Submit a product to the community",
+        to: "/products/submit",
+      },
+      {
+        name: "Promote a Product",
+        description: "Promote a product to the community",
+        to: "/products/promote",
+      },
+    ],
+  },
+  {
+    name: "Jobs",
+    to: "/jobs",
+    items: [
+      {
+        name: "Remote Jobs",
+        description: "See the remote jobs in your community",
+        to: "/jobs?location=remote",
+      },
+      {
+        name: "Full-Time Jobs",
+        description: "See the full-time jobs in your community",
+        to: "/jobs?type=full-time",
+      },
+      {
+        name: "Freelance Jobs",
+        description: "See the freelance jobs in your community",
+        to: "/jobs?type=freelance",
+      },
+      {
+        name: "Internships",
+        description: "See the internships in your community",
+        to: "/jobs?type=internship",
+      },
+      {
+        name: "Submit a Job",
+        description: "Submit a job to the community",
+        to: "/jobs/submit",
+      },
+    ],
+  },
+  {
+    name: "Community",
+    to: "/community",
+    items: [
+      {
+        name: "All Posts",
+        description: "See all posts in the community",
+        to: "/community",
+      },
+      {
+        name: "Top Posts",
+        description: "See the top posts in the community",
+        to: "/community?sort=top",
+      },
+      {
+        name: "New Posts",
+        description: "See the new posts in the community",
+        to: "/community?sort=new",
+      },
+      {
+        name: "Create a Post",
+        description: "Create a post in the community",
+        to: "/community/create",
+      },
+    ],
+  },
+  {
+    name: "IdeasGPT",
+    to: "/ideas",
+  },
+  {
+    name: "Teams",
+    to: "/teams",
+    items: [
+      {
+        name: "All Teams",
+        description: "See all teams in the community",
+        to: "/teams",
+      },
+      {
+        name: "Create a Team",
+        description: "Create a team in the community",
+        to: "/teams/create",
+      },
+    ],
+  },
+];
+
+export function Navigation({
+  isLoggedIn,
+  hasNotifications,
+  hasMessages,
+}: {
+  isLoggedIn: boolean;
+  hasNotifications: boolean;
+  hasMessages: boolean;
+}) {
+  return (
+    <nav className="flex px-20 h-16 items-center justify-between backdrop-blur fixed top-0 left-0 right-0 z-50 bg-background/50">
+      <div className="flex items-center">
+        <Link className="text-lg tracking-tighter font-bold" to="/">
+          Maker
+        </Link>
+        <Separator orientation="vertical" className="h-6 mx-4" />
+        <NavigationMenu>
+          <NavigationMenuList>
+            {menus.map((menu) => (
+              <NavigationMenuItem key={menu.name}>
+                {menu.items ? (
+                  <>
+                    <Link to={menu.to}>
+                      <NavigationMenuTrigger>{menu.name}</NavigationMenuTrigger>
+                    </Link>
+                    <NavigationMenuContent>
+                      <ul className="grid w-[600px] font-light gap-3 p-4 grid-cols-2">
+                        {menu.items?.map((item) => (
+                          <NavigationMenuItem
+                            className={cn([
+                              "select-none rounded-md transition-colors focus:bg-accent hover:bg-accent",
+                              (item.to === "/products/promote" ||
+                                item.to === "/jobs/submit") &&
+                                "col-span-2 bg-primary/10 hover:bg-primary/20 focus:bg-primary/20",
+                            ])}
+                            key={item.name}
+                          >
+                            <NavigationMenuLink>
+                              <Link
+                                className="p-3  space-y-1 block leading-none no-underline outline-none"
+                                to={item.to}
+                              >
+                                <span className="text-sm leading-none">
+                                  {item.name}
+                                </span>
+                                <p className="text-sm leading-snug text-muted-foreground">
+                                  {item.description}
+                                </p>
+                              </Link>
+                            </NavigationMenuLink>
+                          </NavigationMenuItem>
+                        ))}
+                      </ul>
+                    </NavigationMenuContent>
+                  </>
+                ) : (
+                  <Link className={navigationMenuTriggerStyle()} to={menu.to}>
+                    {menu.name}
+                  </Link>
+                )}
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+      {isLoggedIn ? (
+        <div className="flex items-center gap-2">
+          <Button variant="ghost" size="icon" asChild className="relative">
+            <Link to="/my/notifications">
+              <BellIcon className="size-4" />
+              {hasNotifications && (
+                <div className="absolute top-1.5 right-1.5 size-2 rounded-full bg-red-500" />
+              )}
+            </Link>
+          </Button>
+          <Button variant="ghost" size="icon" asChild className="relative">
+            <Link to="/my/messages">
+              <MessageCircleIcon className="size-4" />
+              {hasMessages && (
+                <div className="absolute top-1.5 right-1.5 size-2 rounded-full bg-red-500" />
+              )}
+            </Link>
+          </Button>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Avatar>
+                <AvatarImage src="https://github.com/JongminChoi98.png" />
+                <AvatarFallback>J</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+              <DropdownMenuLabel className="flex flex-col">
+                <span className="font-medium">Joey Choi</span>
+                <span className="text-xs text-muted-foreground">@username</span>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem asChild>
+                  <Link to="/my/dashboard">
+                    <BarChart3Icon className="size-4 mr-2" />
+                    Dashboard
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                  <Link to="/my/profile">
+                    <UserIcon className="size-4 mr-2" />
+                    Profile
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                  <Link to="/my/settings">
+                    <SettingsIcon className="size-4 mr-2" />
+                    Settings
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem asChild>
+                <Link to="/auth/logout">
+                  <LogOutIcon className="size-4 mr-2" />
+                  Logout
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      ) : (
+        <div className="flex items-center gap-4">
+          <Button asChild variant="secondary">
+            <Link to="/auth/login">Login</Link>
+          </Button>
+          <Button asChild>
+            <Link to="/auth/join">Join</Link>
+          </Button>
+        </div>
+      )}
+    </nav>
+  );
+}
