bb981e2edff613125f3e14bef0d9daeb1ad3ba12
Jongmin Choi <138505370+JongminChoi98@users.noreply.github.com>
2025-05-06 20:16:57 -0700

    [ADD] Social Kakao & GitHub join and login

--- app/root.tsx
+++ app/root.tsx
@@ -41,7 +41,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body suppressHydrationWarning>
         <main>{children}</main>
         <ScrollRestoration />
         <Scripts />
